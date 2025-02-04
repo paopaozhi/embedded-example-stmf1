b8486f121bff2d062f89d71518945c31e510f33b
泡泡纸 <paopaozhi@hotmail.com>
2025-02-04 22:17:18 +0800

    build:initial empm build support added

--- app/src/app.c
+++ app/src/app.c
@@ -2,31 +2,77 @@
 #include <string.h>
 
 #include "board.h"
-#include "main.h"
-#include "FreeRTOS.h"
+#include "stm32f1xx_hal.h"
 
-static uint8_t buf[128];
-static uint32_t len = 0;
+UART_InitTypeDef huart1;
 
-const osThreadAttr_t rtc_arrtTask = {
-    .stack_size = 150 * 4,
-    .priority = osPriorityBelowNormal,
-};
+void SystemClock_Config(void);
+void Error_Handler(void);
 
-extern void StartRtcTask(void *arg);
-
-void app(void)
+int main(void)
 {
-    memset(buf, 0, 128);
-    // uart_async_read(uart1, buf, len);
-    // printf("buf:%s\n", buf);
+    HAL_Init();
 
-    osThreadNew(StartRtcTask, NULL, &rtc_arrtTask);
+    SystemClock_Config();
+
+    Board_LED_Init();
 
     while (1)
     {
-        HAL_GPIO_TogglePin(LED0_GPIO_Port, LED0_Pin);
-        printf("free heap:%d\r\n",xPortGetFreeHeapSize());
-        osDelay(1000);
+        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+        HAL_Delay(1000);
     }
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void)
+{
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while (1)
+    {
+    }
+    /* USER CODE END Error_Handler_Debug */
 }
